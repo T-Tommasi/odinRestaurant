a31fee8a03abdb1c5d73b9f1b75a98377ad081f5
T-Tommasi <ttommasi1945@gmail.com>
2024-12-31 11:45:22 +0100

    (FEAT) Added DOM cleansing method
    - Added function for cleansing DOM when switching pages
    - Added event listeners for changing website page

--- src/index.js
+++ src/index.js
@@ -6,9 +6,25 @@ console.log('the JS file has been loaded succesfully')
 //create the heading
 
 function websiteLoader()  {
-    let container = document.querySelector('.content')
-    console.log(container)
-    HomePage(container)
-}
+    let container = document.querySelector('.content');
+    console.log(container);
+    HomePage(container);
 
+    function appendListener(origin,result) {
+        origin.addEventListener('click', result);
+    }
+
+    function cleanseDOM() {
+        const CONTAINER = document.querySelector('.content');
+        while (CONTAINER.firstChild) {
+            CONTAINER.removeChild(CONTAINER.lastChild);
+        }
+        console.log('------');
+        console.log('DOM cleaned')
+    }
+
+    function pageLoader(buttonList) {
+
+    }
+}
 websiteLoader()
